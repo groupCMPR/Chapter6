62a6e08ba29a54ee61a70d72cdb6a9cfb84f145a
groupCMPR <144290496+groupCMPR@users.noreply.github.com>
2023-10-07 19:23:19 -0700

    Update Setupmain.cpp

--- Setupmain.cpp
+++ Setupmain.cpp
@@ -5,10 +5,13 @@
 #include <iostream> //For cout
 #include <vector>   //For vector
 #include <fstream>  //For files
+#include <iomanip> //for setprecision
 
 //HEADER FILES
 #include "input.h" //For input validation
+#include "TemplateBag.h"
 using namespace std;
+
 
 //PROTOTYPES
 int mainMenu();
@@ -93,8 +96,12 @@ void option1()
 	} while (true);
 }
 
+//precondition: called from main
+//postcondition: clears vector, inserts, searches, removes, sorts, and displays
 void option2()
 {
+	MyBag<double, size_t> bag2;
+
 	do
 	{
 		cout << "\n\t2> Template MyBag of doubles";
@@ -112,12 +119,57 @@ void option2()
 		switch (inputChar("\n\t\tOption: ", static_cast<string>("0ABCDEF")))
 		{
 		case '0': return;
-		case 'A': break;
-		case 'B': break;
-		case 'C': break;
-		case 'D': break;
-		case 'E': break;
-		case 'F': break;
+		case 'A': bag2.set_Clear(); cout << "\n\t MyBag is cleared of all elements."; break;
+		case 'B': {
+			double user_Value = inputDouble("\n\tEnter a value and insert into MyBag: ");
+			bag2.set_Insert(user_Value);
+			cout << "\n\t" << user_Value << " has been inserted into MyBag";
+		} break;
+		case 'C': {
+			if (bag2.get_Size() < 1) {
+				cout << "\n\tMyBag is empty";
+				break;
+			}
+
+			double user_Value = (inputDouble("\n\tEnter a value to search from MyBag: ")) * 1.00;
+			int position = bag2.get_Search(user_Value);
+
+			//if not found, it will be a -1, else prints where it was found
+			if (position == -1)
+				cout << "\n\tValue " << user_Value << " is not found from MyBag";
+			else
+				cout << "\n\tValue " << *bag2.get_Value(position) << " is found at subscript #" << position << " from MyBag";
+		} break;
+		case 'D': {
+			if (bag2.get_Size() < 1) {
+				cout << "\n\tMyBag is empty";
+				break;
+			}
+
+			//must use static cast so that inputInteger knows data type
+			double user_Index = inputInteger("\n\tEnter an index(subscript) from MyBag to be deleted: ", 0, static_cast<int>(bag2.get_Size() - 1));
+			cout << "\n\tValue " << *bag2.get_Value(user_Index) <<" has been deleted from MyBag";
+			bag2.set_Remove(user_Index);
+
+		}break;
+		case 'E': {
+			if (bag2.get_Size() < 1) {
+				cout << "\n\tMyBag is empty";
+				break;
+			}
+
+			bag2.set_Sort();
+			cout << "\n\tMybag has been sorted.";
+
+		} break;
+		case 'F': {
+			if (bag2.get_Size() < 1) {
+				cout << "\n\tMyBag is empty";
+				break;
+			}
+
+			cout << "\n\tMybag contains these integers:" << bag2;
+		} break;
 		default: cout << "\t\tERROR: - Invalid option. Please re-enter"; break;
 		}
 		cout << "\n";
